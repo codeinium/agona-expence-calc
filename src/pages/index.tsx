d75682279cc52ab94eccaeccb0944e50bd53ccf8
TheKeilMen <timofiei.tarasov@gmail.com>
2025-04-03 14:49:06 +0300

    finished hw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,6 @@ import {Button} from "@mui/material";
 export const MainLayout: FC = () => {
     return (
         <div className='main-layout'>
-            <h1> Project Expence Calculation </h1>
-
             <nav className='main-navigation'>
                 <Button>
                     <NavLink to='/'> Main </NavLink>
